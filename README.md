48201929c5b72dfb4293d3d30331aa583b8023c0
Masaki Nakahigashi <chuto775@gmail.com>
2017-12-19 01:56:14 +0900

    modify

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 
-# slackapi_x_gas
+# delete_slack_log_gas
 
 ## 概要
 Slack上の、特定チャンネルの、特定日時以前の過去ログを自動で削除する、GoogleAppsScript
@@ -8,15 +8,23 @@ Slack上の、特定チャンネルの、特定日時以前の過去ログを自
 
 Slack Appsを作成する
 1. https://api.slack.com/apps からslack appsを新規作成
-1. 機能＞OAuth & 権限 ページで、スコープを設定する
-1. 設定＞アプリをインストール ページで、インストールする
-1. 機能＞OAuth & 権限 ページで、OAuthアクセストークンをコピーする
+2. 機能＞OAuth & 権限 ページで、スコープを設定する
+3. 設定＞アプリをインストール ページで、インストールする
+4. 機能＞OAuth & 権限 ページで、OAuthアクセストークンをコピーする
 
 Google Apps Script を作成する
 1. [Googleドライブ内](https://drive.google.com/drive/u/0/my-drive)にGoogle Apps Scriptを作成
-1. GASのファイル＞プロジェクトのプロパティ＞スクリプトのプロパティに、OAuthアクセストークンをペースト
+2. GASのファイル＞プロジェクトのプロパティ＞スクリプトのプロパティに、OAuthアクセストークンをペースト
 ```
 名前: slack_oauth_access_token 値: OAuthアクセストークンの値
 ```
-1. src.jsのコードをGASエディター上にペースト
-1. aaa
+3. src.jsのコードを修正
+```js
+  // ログを自動削除するチャンネル名と、何日分のメッセージを残しておくかを設定
+  var objTargetChannels = {
+    'チャンネル名': '14'
+    , 'チャンネル名': '14'
+  }
+```
+4. src.jsのコードをGASエディター上にペースト
+5. GASエディターで、トリガーを設定
--- gas.js
+++ gas.js
@@ -4,14 +4,14 @@ function clearOldMessage() {
   var token = '?token=' + PropertiesService.getScriptProperties().getProperty('slack_oauth_access_token');
 
   // ログを自動削除するチャンネル名と、何日分のメッセージを残しておくかを設定
-  var target_channel_name = '';
-  var target_channel_names = {
-    '': '7',
-    '': '7'
+  var objTargetChannels = {
+    'チャンネル名': '14'
+    , 'チャンネル名': '14'
   }
+
+  // 現在日時
   var date = new Date();
   var now_unix_timestamp = Math.floor( date.getTime() / 1000 );
-  var range_unix_timestamp = now_unix_timestamp - (60 * 60 * 24 * 7); // このタイムスタンプまでのメッセージを削除処理する
 
   // SlackAPIでチャンネルリストを取得
   var method = 'channels.list';
@@ -19,44 +19,46 @@ function clearOldMessage() {
   var response = UrlFetchApp.fetch(exec_api);
   var json = response.getContentText();
   var data = JSON.parse(json);
-  // チャンネルIDを取得
+
   for (var i=0; i<data.channels.length; i++) {
-    if (data.channels[i].name == target_channel_name) {
-      var target_channel_id = data.channels[i].id;
-      break;
+    if (data.channels[i].name in objTargetChannels === true) {
+      var target_channel_name      = data.channels[i].name;
+      var target_channel_id        = data.channels[i].id;
+      var target_channel_time      = objTargetChannels[target_channel_name];
+      var target_channel_timestamp = now_unix_timestamp - (60 * 60 * 24 * target_channel_time);
+      Logger.log(target_channel_time);
+
+      // SlackAPIで、削除対象チャンネルの、削除対象メッセージのタイムスタンプ一覧を取得
+      var method = 'channels.history';
+      var arg1 = '&channel=' + target_channel_id;
+      var arg2 = '&latest=' + target_channel_timestamp;
+      var arg3 = '&count=' + 1000; // 最大で一度に1000件まで削除
+      var exec_api = url + method + token + arg1 + arg2 + arg3;
+      var response = UrlFetchApp.fetch(exec_api);
+      var json = response.getContentText();
+      var data_j = JSON.parse(json);
+      var arrTimeStamp = [];
+      for (var j=0; j<data_j.messages.length; j++) {
+        arrTimeStamp.push(data_j.messages[j]['ts']);
+      }
+
+      // SlackAPIで不要なログを自動削除
+      var method = 'chat.delete';
+      var arg1 = '&channel=' + target_channel_id;
+      for (var k=0; k<arrTimeStamp.length; k++) {
+        var arg2 = '&ts=' + arrTimeStamp[k];
+        var exec_api = url + method + token + arg1 + arg2;
+        UrlFetchApp.fetch(exec_api);
+        Utilities.sleep(1000); // 処理を1秒遅延させる
+      }
+
+      // SlackAPIで削除完了メッセージを投稿
+      var method = 'chat.postMessage';
+      var arg1 = '&channel=%23' + target_channel_name;
+      var arg2 = '&text=' + target_channel_time + '日分以前の過去ログを削除しました';
+      var exec_api = url + method + token + arg1 + arg2;
+      var response = UrlFetchApp.fetch(exec_api);
     }
   }
-
-  // SlackAPIで削除するメッセージのタイムスタンプ一覧を取得
-  var method = 'channels.history';
-  var arg1 = '&channel=' + target_channel_id;
-  var arg2 = '&latest=' + range_unix_timestamp;
-  var arg3 = '&count=' + 1000; // 最大で一度に1000件まで削除
-  var exec_api = url + method + token + arg1 + arg2 + arg3;
-  var response = UrlFetchApp.fetch(exec_api);
-  var json = response.getContentText();
-  var data = JSON.parse(json);
-  var arrTimeStamp = [];
-  for (var i=0; i<data.messages.length; i++) {
-    arrTimeStamp.push(data.messages[i]['ts']);
-  }
-  Logger.log(range_unix_timestamp);
-
-  // SlackAPIで不要なログを自動削除
-  var method = 'chat.delete';
-  var arg1 = '&channel=' + target_channel_id;
-  for (var i=0; i<arrTimeStamp.length; i++) {
-    var arg2 = '&ts=' + arrTimeStamp[i];
-    var exec_api = url + method + token + arg1 + arg2;
-    UrlFetchApp.fetch(exec_api);
-    Utilities.sleep(1000); // 処理を1秒遅延させる
-  }
-
-  // SlackAPIで削除完了メッセージを投稿
-  var method = 'chat.postMessage';
-  var arg1 = '&channel=%23general';
-  var arg2 = '&text=不要な過去ログを削除しました';
-  var exec_api = url + method + token + arg1 + arg2;
-  var response = UrlFetchApp.fetch(exec_api);
 
 }
